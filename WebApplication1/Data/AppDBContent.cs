1788cf2e092718399c2c27200f3eb2c97ca24121
vad7043 <39341964+vad7043@users.noreply.github.com>
2019-11-06 13:56:52 +0400

    Добавил педставление и bootstrap

---
+++ WebApplication1/Data/AppDBContent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Data.Models;
+
+namespace WebApplication1.Data {
+    public class AppDBContent : DbContext{
+        public AppDBContent(DbContextOptions<AppDBContent> options) : base(options){
+
+        }
+        public DbSet<Car> Car { get; set; }
+        public DbSet<Car> Category { get; set; }
+    }
+}
--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -5,20 +5,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebApplication1.Data;
 using WebApplication1.Data.Interfaces;
 using WebApplication1.Data.Mocks;
 
 namespace WebApplication1 {
     public class Startup {
+        private IConfigurationRoot _confString;
+        
+        public Startup(Microsoft.AspNetCore.Hosting.IHostingEnvironment hostEnv) {
+            _confString = new ConfigurationBuilder().SetBasePath(hostEnv.ContentRootPath).AddJsonFile("dbsettings.json").Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
+            services.AddDbContext<AppDBContent>(options => options.UseSqlServer(_confString.GetConnectionString("DefaultConnection")));
             services.AddTransient<IAllCars, MockCars>(); // Позволяет соединить интерфейс и класс, который реализует этот интерфейс
             services.AddTransient<ICarsCategory, MockCategory>();
            // services.AddMvc(); // Подключение MVC
-            services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
